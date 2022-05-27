f9abc21228b0a9508eb91b043440fbd907f5d08a
JanDudasko <101597555+JanDudasko@users.noreply.github.com>
2022-05-27 17:26:17 +0200

    berechnung done

--- solver.py
+++ solver.py
@@ -6,6 +6,8 @@ from mesh_basic import elements_coords, L, elements, nx, ny
 # Given data
 k = 401.
 h_z = 0.001
+q = 1000000
+c = 20.
 
 # Imported geometry
 coords = elements_coords
@@ -38,16 +40,23 @@ def local_stiffness_matrix(element):
     H_e_out = [[(b1*b1+c1*c1)*w,(b1*b2+c1*c2)*w,(b1*b3+c1*c3)*w],
                [(b2*b1+c2*c1)*w,(b2*b2+c2*c2)*w,(b2*b3+c2*c3)*w],
                [(b3*b1+c3*c1)*w,(b3*b2+c3*c2)*w,(b3*b3+c3*c3)*w]]
-    return H_e_out, delta
+    return H_e_out, delta, [b1,b2,b3], [c1,c2,c3]
 
-# Save local stiffness matrices and areas for each element
+# Save local stiffness matrices, geometry coefficients and areas for each element
 H_e = {}
 Area = {}
+coeff_b = {}
+coeff_c = {}
+
 for i in range (len(coords)):
     var = i
     val = local_stiffness_matrix(coords[i])
     H_e[var] = val[0]
     Area[var] = val[1]
+    coeff_b[var] = val[2]
+    coeff_c[var] = val[3]
+
+#print (coeff_b)
 
 ## Global stiffness matrix
 def global_stiffness_matrix(dim_x,dim_y,local_matrices,All_elements):
@@ -74,7 +83,50 @@ def global_stiffness_matrix(dim_x,dim_y,local_matrices,All_elements):
 
 # Save global stiffness matrix
 H = global_stiffness_matrix(nx, ny, H_e, elements)
-print(H)
+#print(H)
+
+##Solution and RHS vectors
+T = np.zeros((nx*nx,1))
+P = np.zeros((ny*ny,1))
 
 ## Boundary conditions
-    # to be done
+# Dirichlet on the upper boundary
+T[nx*nx-nx:nx*nx]=293.
+# Neumann on the lower boundary
+P[0:ny] = q*L*h_z/(2*(nx-1))
+P[1:ny-1] += q*L*h_z/(2*(nx-1))
+#print (P)
+
+## SOLVE
+#Split the system
+H1 = H[0:(nx*nx-(nx)),0:(ny*ny-(ny))]
+H2 = H[0:(nx*nx-(nx)),ny*ny-ny:]
+H3 = H[nx*nx-nx:,0:(ny*ny-(ny))]
+H4 = H[nx*nx-nx:,ny*ny-ny:]
+T1 = T[0:nx*nx-nx]
+T2 = T[nx*nx-nx:]
+P1 = P[0:ny*ny-ny]
+P2 = P[ny*ny-ny:]
+
+# First part
+T1[0:nx*nx-(nx)] = np.linalg.solve(H1,P1-np.matmul(H2,T2))      #using H2 instead of H3 because of dimensions and symmetricity
+
+# Second part
+P2 = np.matmul(H3,T1)+np.matmul(H4,T2)                          #same as above
+
+## Save whole T and P
+T[0:nx*nx-nx] = T1
+P[nx*nx-nx:] = P2
+
+T_elems = {}
+count = 0
+for el in  elements:
+    a = int(el[0])         
+    b = int(el[1])
+    c = int(el[2])
+    T_elems[count] = [float(T[a]),float(T[b]),float(T[c])]
+    count += 1
+print (T_elems)
+
+
+
